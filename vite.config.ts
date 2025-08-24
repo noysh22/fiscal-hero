af7d6f1b0a0ff951e79c8f7a623330db713616c2
Noy Birketvedt-Shimshoni <4431921+noysh22@users.noreply.github.com>
2025-08-24 22:26:56 +0200

    Fix url name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Set base path for GitHub Pages deployment
-  base: mode === 'production' ? '/cyber-fiscal-flux/' : '/',
+  base: mode === 'production' ? '/fiscal-hero/' : '/',
   server: {
     host: "::",
     port: 8080,
